f64d0a6efdb1e1541e9ed9bb9f9ebd06c1dc8efd
AshitakaSekki <constantine.sky@hotmail.com>
2019-01-05 02:03:55 +0800

    项目配置更新
    1. 修改缩进为4个空格

--- Vector_t.h
+++ Vector_t.h
@@ -8,145 +8,145 @@
 class vector_t
 {
 public:
-	float x, y, z, w;
-	vector_t() :x(0.0f), y(0.0f), z(0.0f), w(1.0f) {}
-	vector_t(const vector_t &a) :x(a.x), y(a.y), z(a.z), w(a.w) {}
-	vector_t(float nx, float ny, float nz) :x(nx), y(ny), z(nz), w(1.0f) {}
+    float x, y, z, w;
+    vector_t() :x(0.0f), y(0.0f), z(0.0f), w(1.0f) {}
+    vector_t(const vector_t &a) :x(a.x), y(a.y), z(a.z), w(a.w) {}
+    vector_t(float nx, float ny, float nz) :x(nx), y(ny), z(nz), w(1.0f) {}
 
-	// 重载赋值运算符并返回引用
-	vector_t& operator=(const vector_t &a)
-	{
-		x = a.x; y = a.y; z = a.z; w = a.w;
-		return *this;
-	}
+    // 重载赋值运算符并返回引用
+    vector_t& operator=(const vector_t &a)
+    {
+        x = a.x; y = a.y; z = a.z; w = a.w;
+        return *this;
+    }
 
-	bool operator==(const vector_t &a) const
-	{
-		return x == a.x && y == a.y && z == a.z && w == a.w;
-	}
+    bool operator==(const vector_t &a) const
+    {
+        return x == a.x && y == a.y && z == a.z && w == a.w;
+    }
 
-	bool operator!=(const vector_t &a) const
-	{
-		return x != a.x || y != a.y || z != a.z || w != a.w;
-	}
+    bool operator!=(const vector_t &a) const
+    {
+        return x != a.x || y != a.y || z != a.z || w != a.w;
+    }
 
-	// 置为零向量
-	void zero() { x = y = z = 0.0f; }
+    // 置为零向量
+    void zero() { x = y = z = 0.0f; }
 
-	vector_t operator-() const
-	{
-		return vector_t(-x, -y, -z);
-	}
+    vector_t operator-() const
+    {
+        return vector_t(-x, -y, -z);
+    }
 
-	vector_t operator+(const vector_t &a) const
-	{
-		return vector_t(x + a.x, y + a.y, z + a.z);
-	}
+    vector_t operator+(const vector_t &a) const
+    {
+        return vector_t(x + a.x, y + a.y, z + a.z);
+    }
 
-	vector_t operator-(const vector_t &a) const
-	{
-		return vector_t(x - a.x, y - a.y, z - a.z);
-	}
+    vector_t operator-(const vector_t &a) const
+    {
+        return vector_t(x - a.x, y - a.y, z - a.z);
+    }
 
-	// 向量与标量相乘，重载"*"运算符
-	vector_t operator*(float a)const
-	{
-		return vector_t(x*a, y*a, z*a);
-	}
+    // 向量与标量相乘，重载"*"运算符
+    vector_t operator*(float a)const
+    {
+        return vector_t(x*a, y*a, z*a);
+    }
 
-	// 向量与矩阵相乘，重载"*"运算符
-	vector_t operator*(const matrix_t &m) const
-	{
-		vector_t v;
-		v.x = x * m.m[0][0] + y * m.m[1][0] + z * m.m[2][0] + w * m.m[3][0];
-		v.y = x * m.m[0][1] + y * m.m[1][1] + z * m.m[2][1] + w * m.m[3][1];
-		v.z = x * m.m[0][2] + y * m.m[1][2] + z * m.m[2][2] + w * m.m[3][2];
-		v.w = x * m.m[0][3] + y * m.m[1][3] + z * m.m[2][3] + w * m.m[3][3];
-		return v;
-	}
+    // 向量与矩阵相乘，重载"*"运算符
+    vector_t operator*(const matrix_t &m) const
+    {
+        vector_t v;
+        v.x = x * m.m[0][0] + y * m.m[1][0] + z * m.m[2][0] + w * m.m[3][0];
+        v.y = x * m.m[0][1] + y * m.m[1][1] + z * m.m[2][1] + w * m.m[3][1];
+        v.z = x * m.m[0][2] + y * m.m[1][2] + z * m.m[2][2] + w * m.m[3][2];
+        v.w = x * m.m[0][3] + y * m.m[1][3] + z * m.m[2][3] + w * m.m[3][3];
+        return v;
+    }
 
-	vector_t operator/(float a)const
-	{
-		float oneOverA = 1.0f / a;
-		return vector_t(x*oneOverA, y*oneOverA, z*oneOverA);
-	}
+    vector_t operator/(float a)const
+    {
+        float oneOverA = 1.0f / a;
+        return vector_t(x*oneOverA, y*oneOverA, z*oneOverA);
+    }
 
-	vector_t& operator+=(const vector_t &a)
-	{
-		x += a.x; y += a.y; z += a.z;
-		return *this;
-	}
+    vector_t& operator+=(const vector_t &a)
+    {
+        x += a.x; y += a.y; z += a.z;
+        return *this;
+    }
 
-	vector_t& operator-=(const vector_t &a)
-	{
-		x -= a.x; y -= a.y; z -= a.z;
-		return *this;
-	}
+    vector_t& operator-=(const vector_t &a)
+    {
+        x -= a.x; y -= a.y; z -= a.z;
+        return *this;
+    }
 
-	vector_t& operator*=(float a)
-	{
-		x *= a; y *= a; z *= a;
-		return *this;
-	}
+    vector_t& operator*=(float a)
+    {
+        x *= a; y *= a; z *= a;
+        return *this;
+    }
 
-	vector_t& operator/=(float a)
-	{
-		float oneOverA = 1.0f / a;
-		x *= oneOverA; y *= oneOverA; z *= oneOverA;
-		return *this;
-	}
+    vector_t& operator/=(float a)
+    {
+        float oneOverA = 1.0f / a;
+        x *= oneOverA; y *= oneOverA; z *= oneOverA;
+        return *this;
+    }
 
-	friend inline float vectorMag(const vector_t &a);
+    friend inline float vectorMag(const vector_t &a);
 
-	// 向量标准化
-	void normalize()
-	{
-		float length = vectorMag(*this);
-		if (length != 0.0f) {
-			float inv = 1.0f / length;
-			x *= inv;
-			y *= inv;
-			z *= inv;
-		}
-	}
+    // 向量标准化
+    void normalize()
+    {
+        float length = vectorMag(*this);
+        if (length != 0.0f) {
+            float inv = 1.0f / length;
+            x *= inv;
+            y *= inv;
+            z *= inv;
+        }
+    }
 
-	// 向量点乘,重载"*"运算符
-	float operator*(const vector_t &a) const
-	{
-		return x * a.x + y * a.y + z * a.z;
-	}
+    // 向量点乘,重载"*"运算符
+    float operator*(const vector_t &a) const
+    {
+        return x * a.x + y * a.y + z * a.z;
+    }
 };
 
 // 向量模长
 inline float vectorMag(const vector_t &a)
 {
-	float sq = a.x * a.x + a.y * a.y + a.z * a.z;
-	return (float)sqrt(sq);
+    float sq = a.x * a.x + a.y * a.y + a.z * a.z;
+    return (float)sqrt(sq);
 }
 
 // 向量叉乘
 inline vector_t crossProduct(const vector_t &a, const vector_t &b)
 {
-	return vector_t(
-		a.y * b.z - a.z * b.y,
-		a.z * b.x - a.x * b.z,
-		a.x * b.y - a.y * b.x
-	);
+    return vector_t(
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    );
 }
 
 // 标量左乘，重载"*"运算符
 inline vector_t operator*(float k, const vector_t &v)
 {
-	return vector_t(k*v.x, k*v.y, k*v.z);
+    return vector_t(k*v.x, k*v.y, k*v.z);
 }
 
 // 向量距离
 inline float distance(const vector_t &a, const vector_t &b)
 {
-	float dx = a.x - b.x;
-	float dy = a.y - b.y;
-	float dz = a.z - a.z;
-	return (float)sqrt(dx*dx + dy * dy + dz * dz);
+    float dx = a.x - b.x;
+    float dy = a.y - b.y;
+    float dz = a.z - a.z;
+    return (float)sqrt(dx*dx + dy * dy + dz * dz);
 }
 
 // 计算插值：t 为 [0, 1] 之间的数值
@@ -155,11 +155,11 @@ float interp(float x1, float x2, float t);
 // 向量插值，t取值 [0, 1]
 inline vector_t vectorInterp(const vector_t &a, const vector_t &b, float t)
 {
-	return vector_t(
-		interp(a.x, b.x, t),
-		interp(a.y, b.y, t),
-		interp(a.z, b.z, t)
-	);
+    return vector_t(
+        interp(a.x, b.x, t),
+        interp(a.y, b.y, t),
+        interp(a.z, b.z, t)
+    );
 }
 
 
--- matrix_t.h
+++ matrix_t.h
@@ -1,4 +1,4 @@
-#ifndef _MATRIX_T_H_
+ï»¿#ifndef _MATRIX_T_H_
 #define _MATRIX_T_H_
 
 #include "vector_t.h"
@@ -6,161 +6,161 @@
 class matrix_t
 {
 public:
-	float **m;
-	void initialize()
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j=0;j<4;j++)
-			{
-				m[i][j] = 0.0f;
-			}
-		}
-	}
+    float **m;
+    void initialize()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                m[i][j] = 0.0f;
+            }
+        }
+    }
 
-	matrix_t() { initialize(); }
-	matrix_t(const matrix_t &a) :m(a.m) {}
+    matrix_t() { initialize(); }
+    matrix_t(const matrix_t &a) :m(a.m) {}
 
-	// ÖØÔØ¸³ÖµÔËËã·û²¢·µ»ØÒýÓÃ
-	matrix_t& operator=(const matrix_t &a)
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				m[i][j] = a.m[i][j];
-			}
-		}
-		return *this;
-	}
+    // é‡è½½èµ‹å€¼è¿ç®—ç¬¦å¹¶è¿”å›žå¼•ç”¨
+    matrix_t& operator=(const matrix_t &a)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                m[i][j] = a.m[i][j];
+            }
+        }
+        return *this;
+    }
 
-	matrix_t operator+(const matrix_t &a) const
-	{
-		matrix_t n;
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				n.m[i][j] = m[i][j] + a.m[i][j];
-			}
-		}
-		return n;
-	}
+    matrix_t operator+(const matrix_t &a) const
+    {
+        matrix_t n;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                n.m[i][j] = m[i][j] + a.m[i][j];
+            }
+        }
+        return n;
+    }
 
-	matrix_t operator-(const matrix_t &a) const
-	{
-		matrix_t z;
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				z.m[i][j] = m[i][j] - a.m[i][j];
-			}
-		}
-		return z;
-	}
+    matrix_t operator-(const matrix_t &a) const
+    {
+        matrix_t z;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                z.m[i][j] = m[i][j] - a.m[i][j];
+            }
+        }
+        return z;
+    }
 
-	// ¾ØÕóÏà³Ë£¬ÖØÔØ"*"ÔËËã·û
-	matrix_t operator*(const matrix_t &a) const
-	{
-		matrix_t z;
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				z.m[j][i] = (m[j][0] * a.m[0][i]) +
-							(m[j][1] * a.m[1][i]) +
-							(m[j][2] * a.m[2][i]) +
-							(m[j][3] * a.m[3][i]);
-			}
-		}
-		return z;
-	}
+    // çŸ©é˜µç›¸ä¹˜ï¼Œé‡è½½"*"è¿ç®—ç¬¦
+    matrix_t operator*(const matrix_t &a) const
+    {
+        matrix_t z;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                z.m[j][i] = (m[j][0] * a.m[0][i]) +
+                    (m[j][1] * a.m[1][i]) +
+                    (m[j][2] * a.m[2][i]) +
+                    (m[j][3] * a.m[3][i]);
+            }
+        }
+        return z;
+    }
 
-	// ¾ØÕóÓë±êÁ¿Ïà³Ë£¬ÖØÔØ"*"ÔËËã·û
-	matrix_t operator*(float f) const
-	{
-		matrix_t z;
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				z.m[i][j] = m[i][j] * f;
-			}
-		}
-		return z;
-	}
+    // çŸ©é˜µä¸Žæ ‡é‡ç›¸ä¹˜ï¼Œé‡è½½"*"è¿ç®—ç¬¦
+    matrix_t operator*(float f) const
+    {
+        matrix_t z;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                z.m[i][j] = m[i][j] * f;
+            }
+        }
+        return z;
+    }
 
 
-	// ÉèÎªµ¥Î»¾ØÕó
-	void setIdentity()
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				if (i==j)
-				{
-					m[i][j] = 1.0f;
-				}
-				else
-				{
-					m[i][j] = 0.0f;
-				}
-			}
-		}
-	}
+    // è®¾ä¸ºå•ä½çŸ©é˜µ
+    void setIdentity()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (i == j)
+                {
+                    m[i][j] = 1.0f;
+                }
+                else
+                {
+                    m[i][j] = 0.0f;
+                }
+            }
+        }
+    }
 
-	// ÉèÎªÁã¾ØÕó
-	void setZero()
-	{
-		initialize();
-	}
+    // è®¾ä¸ºé›¶çŸ©é˜µ
+    void setZero()
+    {
+        initialize();
+    }
 
-	// Æ½ÒÆ±ä»»
-	void setTranslate(float x, float y, float z)
-	{
-		setIdentity();
-		m[3][0] = x;
-		m[3][1] = y;
-		m[3][2] = z;
-	}
+    // å¹³ç§»å˜æ¢
+    void setTranslate(float x, float y, float z)
+    {
+        setIdentity();
+        m[3][0] = x;
+        m[3][1] = y;
+        m[3][2] = z;
+    }
 
-	// Ëõ·Å±ä»»
-	void setScale(float x, float y, float z) 
-	{
-		setIdentity();
-		m[0][0] = x;
-		m[1][1] = y;
-		m[2][2] = z;
-	}
+    // ç¼©æ”¾å˜æ¢
+    void setScale(float x, float y, float z)
+    {
+        setIdentity();
+        m[0][0] = x;
+        m[1][1] = y;
+        m[2][2] = z;
+    }
 
-	// Ðý×ª±ä»»
-	void setRotate(float x, float y, float z, float theta)
-	{
-		float qsin = (float)sin(theta * 0.5f);
-		float qcos = (float)cos(theta * 0.5f);
-		//vector_t vec = { x, y, z, 1.0f };
-		vector_t vec = vector_t(x, y, z);
+    // æ—‹è½¬å˜æ¢
+    void setRotate(float x, float y, float z, float theta)
+    {
+        float qsin = (float)sin(theta * 0.5f);
+        float qcos = (float)cos(theta * 0.5f);
+        //vector_t vec = { x, y, z, 1.0f };
+        vector_t vec = vector_t(x, y, z);
 
-		float w = qcos;
-		vec.normalize();
-		x = vec.x * qsin;
-		y = vec.y * qsin;
-		z = vec.z * qsin;
-		m[0][0] = 1 - 2 * y * y - 2 * z * z;
-		m[1][0] = 2 * x * y - 2 * w * z;
-		m[2][0] = 2 * x * z + 2 * w * y;
-		m[0][1] = 2 * x * y + 2 * w * z;
-		m[1][1] = 1 - 2 * x * x - 2 * z * z;
-		m[2][1] = 2 * y * z - 2 * w * x;
-		m[0][2] = 2 * x * z - 2 * w * y;
-		m[1][2] = 2 * y * z + 2 * w * x;
-		m[2][2] = 1 - 2 * x * x - 2 * y * y;
-		m[0][3] = m[1][3] = m[2][3] = 0.0f;
-		m[3][0] = m[3][1] = m[3][2] = 0.0f;
-		m[3][3] = 1.0f;
-	}
+        float w = qcos;
+        vec.normalize();
+        x = vec.x * qsin;
+        y = vec.y * qsin;
+        z = vec.z * qsin;
+        m[0][0] = 1 - 2 * y * y - 2 * z * z;
+        m[1][0] = 2 * x * y - 2 * w * z;
+        m[2][0] = 2 * x * z + 2 * w * y;
+        m[0][1] = 2 * x * y + 2 * w * z;
+        m[1][1] = 1 - 2 * x * x - 2 * z * z;
+        m[2][1] = 2 * y * z - 2 * w * x;
+        m[0][2] = 2 * x * z - 2 * w * y;
+        m[1][2] = 2 * y * z + 2 * w * x;
+        m[2][2] = 1 - 2 * x * x - 2 * y * y;
+        m[0][3] = m[1][3] = m[2][3] = 0.0f;
+        m[3][0] = m[3][1] = m[3][2] = 0.0f;
+        m[3][3] = 1.0f;
+    }
 };
 
 #endif // !_MATRIX_T_H_
