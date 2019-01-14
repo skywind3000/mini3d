bf6b40651a9d05a46f05c70df5279ae232a8863e
AshitakaSekki <constantine.sky@hotmail.com>
2019-01-14 18:54:07 +0800

    Update mini3d.cpp
    1.  注释掉原来的winapi窗口，改由sdl创建窗口

--- mini3d.cpp
+++ mini3d.cpp
@@ -31,7 +31,8 @@
 typedef unsigned int IUINT32;
 
 // 窗口分辨率
-const int 
+const IUINT32 SCREEN_WIDTH  = 800;
+const IUINT32 SCREEN_HEIGHT = 600;
 
 //=====================================================================
 // 数学库：此部分应该不用详解，熟悉 D3D 矩阵变换即可
@@ -853,133 +854,133 @@ void device_draw_primitive(device_t *device, const vertex_t *v1,
 
 
 
-//=====================================================================
-// Win32 窗口及图形绘制：为 device 提供一个 DibSection 的 FB
-//=====================================================================
-int screen_w, screen_h, screen_exit = 0;
-int screen_mx = 0, screen_my = 0, screen_mb = 0;
-int screen_keys[512];	// 当前键盘按下状态
-static HWND screen_handle = NULL;		// 主窗口 HWND
-static HDC screen_dc = NULL;			// 配套的 HDC
-static HBITMAP screen_hb = NULL;		// DIB
-static HBITMAP screen_ob = NULL;		// 老的 BITMAP
-unsigned char *screen_fb = NULL;		// frame buffer
-long screen_pitch = 0;
-
-int screen_init(int w, int h, const TCHAR *title);	// 屏幕初始化
-int screen_close(void);								// 关闭屏幕
-void screen_dispatch(void);							// 处理消息
-void screen_update(void);							// 显示 FrameBuffer
-
-// win32 event handler
-static LRESULT screen_events(HWND, UINT, WPARAM, LPARAM);
-
-#ifdef _MSC_VER
-#pragma comment(lib, "gdi32.lib")
-#pragma comment(lib, "user32.lib")
-#endif
-
-// 初始化窗口并设置标题
-int screen_init(int w, int h, const TCHAR *title) {
-    WNDCLASS wc = { CS_BYTEALIGNCLIENT, (WNDPROC)screen_events, 0, 0, 0,
-        NULL, NULL, NULL, NULL, _T("SCREEN3.1415926") };
-    BITMAPINFO bi = { { sizeof(BITMAPINFOHEADER), w, -h, 1, 32, BI_RGB,
-        w * h * 4, 0, 0, 0, 0 } };
-    RECT rect = { 0, 0, w, h };
-    int wx, wy, sx, sy;
-    LPVOID ptr;
-    HDC hDC;
-
-    screen_close();
-
-    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-    wc.hInstance = GetModuleHandle(NULL);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    if (!RegisterClass(&wc)) return -1;
-
-    screen_handle = CreateWindow(_T("SCREEN3.1415926"), title,
-        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-        0, 0, 0, 0, NULL, NULL, wc.hInstance, NULL);
-    if (screen_handle == NULL) return -2;
-
-    screen_exit = 0;
-    hDC = GetDC(screen_handle);
-    screen_dc = CreateCompatibleDC(hDC);
-    ReleaseDC(screen_handle, hDC);
-
-    screen_hb = CreateDIBSection(screen_dc, &bi, DIB_RGB_COLORS, &ptr, 0, 0);
-    if (screen_hb == NULL) return -3;
-
-    screen_ob = (HBITMAP)SelectObject(screen_dc, screen_hb);
-    screen_fb = (unsigned char*)ptr;
-    screen_w = w;
-    screen_h = h;
-    screen_pitch = w * 4;
-
-    AdjustWindowRect(&rect, GetWindowLong(screen_handle, GWL_STYLE), 0);
-    wx = rect.right - rect.left;
-    wy = rect.bottom - rect.top;
-    sx = (GetSystemMetrics(SM_CXSCREEN) - wx) / 2;
-    sy = (GetSystemMetrics(SM_CYSCREEN) - wy) / 2;
-    if (sy < 0) sy = 0;
-    SetWindowPos(screen_handle, NULL, sx, sy, wx, wy, (SWP_NOCOPYBITS | SWP_NOZORDER | SWP_SHOWWINDOW));
-    SetForegroundWindow(screen_handle);
-
-    ShowWindow(screen_handle, SW_NORMAL);
-    screen_dispatch();
-
-    memset(screen_keys, 0, sizeof(int) * 512);
-    memset(screen_fb, 0, w * h * 4);
-
-    return 0;
-}
-
-int screen_close(void) {
-    if (screen_dc) {
-        if (screen_ob) {
-            SelectObject(screen_dc, screen_ob);
-            screen_ob = NULL;
-        }
-        DeleteDC(screen_dc);
-        screen_dc = NULL;
-    }
-    if (screen_hb) {
-        DeleteObject(screen_hb);
-        screen_hb = NULL;
-    }
-    if (screen_handle) {
-        CloseWindow(screen_handle);
-        screen_handle = NULL;
-    }
-    return 0;
-}
-
-static LRESULT screen_events(HWND hWnd, UINT msg,
-    WPARAM wParam, LPARAM lParam) {
-    switch (msg) {
-    case WM_CLOSE: screen_exit = 1; break;
-    case WM_KEYDOWN: screen_keys[wParam & 511] = 1; break;
-    case WM_KEYUP: screen_keys[wParam & 511] = 0; break;
-    default: return DefWindowProc(hWnd, msg, wParam, lParam);
-    }
-    return 0;
-}
-
-void screen_dispatch(void) {
-    MSG msg;
-    while (1) {
-        if (!PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE)) break;
-        if (!GetMessage(&msg, NULL, 0, 0)) break;
-        DispatchMessage(&msg);
-    }
-}
-
-void screen_update(void) {
-    HDC hDC = GetDC(screen_handle);
-    BitBlt(hDC, 0, 0, screen_w, screen_h, screen_dc, 0, 0, SRCCOPY);
-    ReleaseDC(screen_handle, hDC);
-    screen_dispatch();
-}
+////=====================================================================
+//// Win32 窗口及图形绘制：为 device 提供一个 DibSection 的 FB
+////=====================================================================
+//int screen_w, screen_h, screen_exit = 0;
+//int screen_mx = 0, screen_my = 0, screen_mb = 0;
+//int screen_keys[512];	// 当前键盘按下状态
+//static HWND screen_handle = NULL;		// 主窗口 HWND
+//static HDC screen_dc = NULL;			// 配套的 HDC
+//static HBITMAP screen_hb = NULL;		// DIB
+//static HBITMAP screen_ob = NULL;		// 老的 BITMAP
+//unsigned char *screen_fb = NULL;		// frame buffer
+//long screen_pitch = 0;
+//
+//int screen_init(int w, int h, const TCHAR *title);	// 屏幕初始化
+//int screen_close(void);								// 关闭屏幕
+//void screen_dispatch(void);							// 处理消息
+//void screen_update(void);							// 显示 FrameBuffer
+//
+//// win32 event handler
+//static LRESULT screen_events(HWND, UINT, WPARAM, LPARAM);
+//
+//#ifdef _MSC_VER
+//#pragma comment(lib, "gdi32.lib")
+//#pragma comment(lib, "user32.lib")
+//#endif
+//
+//// 初始化窗口并设置标题
+//int screen_init(int w, int h, const TCHAR *title) {
+//    WNDCLASS wc = { CS_BYTEALIGNCLIENT, (WNDPROC)screen_events, 0, 0, 0,
+//        NULL, NULL, NULL, NULL, _T("SCREEN3.1415926") };
+//    BITMAPINFO bi = { { sizeof(BITMAPINFOHEADER), w, -h, 1, 32, BI_RGB,
+//        w * h * 4, 0, 0, 0, 0 } };
+//    RECT rect = { 0, 0, w, h };
+//    int wx, wy, sx, sy;
+//    LPVOID ptr;
+//    HDC hDC;
+//
+//    screen_close();
+//
+//    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+//    wc.hInstance = GetModuleHandle(NULL);
+//    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+//    if (!RegisterClass(&wc)) return -1;
+//
+//    screen_handle = CreateWindow(_T("SCREEN3.1415926"), title,
+//        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+//        0, 0, 0, 0, NULL, NULL, wc.hInstance, NULL);
+//    if (screen_handle == NULL) return -2;
+//
+//    screen_exit = 0;
+//    hDC = GetDC(screen_handle);
+//    screen_dc = CreateCompatibleDC(hDC);
+//    ReleaseDC(screen_handle, hDC);
+//
+//    screen_hb = CreateDIBSection(screen_dc, &bi, DIB_RGB_COLORS, &ptr, 0, 0);
+//    if (screen_hb == NULL) return -3;
+//
+//    screen_ob = (HBITMAP)SelectObject(screen_dc, screen_hb);
+//    screen_fb = (unsigned char*)ptr;
+//    screen_w = w;
+//    screen_h = h;
+//    screen_pitch = w * 4;
+//
+//    AdjustWindowRect(&rect, GetWindowLong(screen_handle, GWL_STYLE), 0);
+//    wx = rect.right - rect.left;
+//    wy = rect.bottom - rect.top;
+//    sx = (GetSystemMetrics(SM_CXSCREEN) - wx) / 2;
+//    sy = (GetSystemMetrics(SM_CYSCREEN) - wy) / 2;
+//    if (sy < 0) sy = 0;
+//    SetWindowPos(screen_handle, NULL, sx, sy, wx, wy, (SWP_NOCOPYBITS | SWP_NOZORDER | SWP_SHOWWINDOW));
+//    SetForegroundWindow(screen_handle);
+//
+//    ShowWindow(screen_handle, SW_NORMAL);
+//    screen_dispatch();
+//
+//    memset(screen_keys, 0, sizeof(int) * 512);
+//    memset(screen_fb, 0, w * h * 4);
+//
+//    return 0;
+//}
+//
+//int screen_close(void) {
+//    if (screen_dc) {
+//        if (screen_ob) {
+//            SelectObject(screen_dc, screen_ob);
+//            screen_ob = NULL;
+//        }
+//        DeleteDC(screen_dc);
+//        screen_dc = NULL;
+//    }
+//    if (screen_hb) {
+//        DeleteObject(screen_hb);
+//        screen_hb = NULL;
+//    }
+//    if (screen_handle) {
+//        CloseWindow(screen_handle);
+//        screen_handle = NULL;
+//    }
+//    return 0;
+//}
+//
+//static LRESULT screen_events(HWND hWnd, UINT msg,
+//    WPARAM wParam, LPARAM lParam) {
+//    switch (msg) {
+//    case WM_CLOSE: screen_exit = 1; break;
+//    case WM_KEYDOWN: screen_keys[wParam & 511] = 1; break;
+//    case WM_KEYUP: screen_keys[wParam & 511] = 0; break;
+//    default: return DefWindowProc(hWnd, msg, wParam, lParam);
+//    }
+//    return 0;
+//}
+//
+//void screen_dispatch(void) {
+//    MSG msg;
+//    while (1) {
+//        if (!PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE)) break;
+//        if (!GetMessage(&msg, NULL, 0, 0)) break;
+//        DispatchMessage(&msg);
+//    }
+//}
+//
+//void screen_update(void) {
+//    HDC hDC = GetDC(screen_handle);
+//    BitBlt(hDC, 0, 0, screen_w, screen_h, screen_dc, 0, 0, SRCCOPY);
+//    ReleaseDC(screen_handle, hDC);
+//    screen_dispatch();
+//}
 
 
 //=====================================================================
@@ -1045,9 +1046,36 @@ void init_texture(device_t *device) {
     device_set_texture(device, texture, 256 * 4, 256, 256);
 }
 
-int main(void)
+int main(int argc, char* args[])
 {
-
+    // 创建SDL窗口
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+    // 初始化窗口
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        // 创建窗口
+        window = SDL_CreateWindow("Mini3d (software render tutorial) - Left/Right: rotation, Up/Down: forward/backward, Space: switch state", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            // 创建表面
+            screenSurface = SDL_GetWindowSurface(window);
+            // 填充颜色（全白）
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xff, 0xff, 0xff));
+            // 更新窗口表面
+            SDL_UpdateWindowSurface(window);
+            // 延迟两秒
+            SDL_Delay(2000);
+        }
+    }
 
     device_t device;
     int states[] = { RENDER_STATE_TEXTURE, RENDER_STATE_COLOR, RENDER_STATE_WIREFRAME };
@@ -1113,6 +1141,12 @@ int main(void)
         screen_update();
         Sleep(1);
     }
+
+    // 销毁窗口
+    SDL_DestroyWindow(window);
+    // 退出SDL子系统
+    SDL_Quit();
+
     return 0;
 }
 
